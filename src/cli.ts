7e99ed10a9cb133b0948c692a8797a7075f2fbab
Basavanagowda Kanur <basavanagowda@gmail.com>
2020-03-26 16:45:37 +0100

    Implement command-line executable

---
+++ src/cli.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import fs from 'fs';
+import compiler from './compiler';
+
+const [,,...args] = process.argv;
+if (args.length === 0) {
+  // No file specified, wait for stdin
+  const input = fs.readFileSync('/dev/stdin', {encoding: 'utf8'});
+  const output = compiler(input);
+  console.log(output); // tslint:disable-line no-console
+} else {
+  // Iterate through each file & compile them
+  args.forEach(arg => {
+    try {
+      fs.accessSync(arg, fs.constants.R_OK);
+
+      const input = fs.readFileSync(arg, {encoding: 'utf8'});
+      const output = compiler(input);
+
+      console.log(`${output}`); // tslint:disable-line no-console
+
+    } catch (err) {
+      console.log(`${arg}: No such file or directory`); // tslint:disable-line no-console
+      process.exit(127);
+    }
+  });
+}
